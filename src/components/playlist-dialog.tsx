2a6bcdcaf8828d845be8114f65be6908514ab33b
Werison Pereira <pereirawerison@gmail.com>
2025-08-26 15:44:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/playlist-dialog.tsx
+++ src/components/playlist-dialog.tsx
@@ -22,7 +22,7 @@ import { cn } from '@/lib/utils';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
 import { Badge } from './ui/badge';
-import { useIsMobile } from '@/hooks/use-is-mobile';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 
 interface PlaylistDialogProps {
