d79d8f7bf4cdf79d7eb2bfcfd2120d941bf52695
Werison Pereira <pereirawerison@gmail.com>
2026-01-25 21:21:54 +0000

    Error saving member: FirebaseError: Firebase Storage: No default bucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:983918160630:web:afbbaf6ba6f3ef9b10dad7",
   "apiKey": "AIzaSyBaIjOhtTvjmHSmIHZYtRyCSxVgX_P4BRc",
   "authDomain": "louvor-icabv.firebaseapp.com",
+  "storageBucket": "louvor-icabv.appspot.com",
   "measurementId": "",
   "messagingSenderId": "983918160630"
 };
