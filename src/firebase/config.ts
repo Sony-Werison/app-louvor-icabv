e1e1a52629882ac95ec380f20ab9ba8ba081c63e
Werison Pereira <pereirawerison@gmail.com>
2026-01-25 19:26:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "louvor-icabv",
+  "appId": "1:983918160630:web:afbbaf6ba6f3ef9b10dad7",
+  "apiKey": "AIzaSyBaIjOhtTvjmHSmIHZYtRyCSxVgX_P4BRc",
+  "authDomain": "louvor-icabv.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "983918160630"
+};
