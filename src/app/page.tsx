19cce1f78c929ce9f77a329b8e9a41e866729139
Werison Pereira <pereirawerison@gmail.com>
2026-02-27 14:25:38 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import Link from 'next/navigation';
+import Link from 'next/link';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   ListMusic,
